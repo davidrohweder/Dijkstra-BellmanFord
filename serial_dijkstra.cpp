1e737231b01e91fa92addd5acb8b4bb06a2f28d8
David Rohweder <david01rohweder@gmail.com>
2022-04-15 12:13:53 -0700

    update

--- serial_dijkstra.cpp
+++ serial_dijkstra.cpp
@@ -21,33 +21,42 @@
 // **** Function Region
 
 
+int smallest_Distance(int distance[], bool visited[])
+{
+    int minimum = INT_MAX , idx;
+
+    for(int i = 0; i < N; i++) 
+    {
+        if(visited[i] == false && distance[i] <= minimum)      
+        {
+            minimum = distance[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+
+
 /*
     Naive, Serial Implementation of Dijksta's Algorithm... O(|V|) runtime and O(|V|*|E|) storage 
 */
 void serial_Dijkstra(Graph* graph)
 {
     bool* visited = new bool[N];
-    
+
     for(int i = 0; i < N; i++) {
         graph->serial_distance[i] = INT_MAX;
         visited[i] = false;    
     } // initalize all elements in distance vector to "infinity"
-    
+
     graph->serial_distance[graph->src] = 0; // source vertex has distance 0               
-    
+
     for(int i = 0; i < N; i++) {
-        int s, min;
-        for (int z = 0; z < N; z++) {
-            if(visited[z] == false && graph->serial_distance[z] <= min) {
-                min = graph->serial_distance[z];
-                s = z;
-            }
-        } // find vertex with shortest minimum distance
-        
-        visited[s] = true;
+        int s = smallest_Distance(graph->serial_distance,visited); 
+        visited[s]=true;
 
         for(int j = 0; j < N; j++) {
-            if(!(visited[j]) && graph->graph[s * N + j] && graph->serial_distance[s] != INT_MAX && graph->serial_distance[s] + graph->graph[s * N + j] < graph->serial_distance[j]) {
+            if(!visited[j] && graph->graph[s * N + j] && graph->serial_distance[s] != INT_MAX && graph->serial_distance[s] + graph->graph[s * N + j] < graph->serial_distance[j]) {
                 graph->serial_distance[j]= graph->serial_distance[s] + graph->graph[s * N + j];
             }
         } // updating the distance of neighbouring vertex
@@ -56,4 +65,4 @@ void serial_Dijkstra(Graph* graph)
     // Cleanup
     delete[] visited;
 
-}
+} 
